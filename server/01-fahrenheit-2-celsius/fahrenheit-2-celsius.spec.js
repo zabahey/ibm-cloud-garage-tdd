4ed51c4e267816516719ae429dd1e96759a47a5c
Parinya Samrongsap <parinya.s@cakeboxsolutions.com>
2019-10-31 11:11:47 +0700

    design the behavior of our fahrenheit to celsius calculator

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-const { fahrenheitToCelcius } = require('./fahrenheit-2-celcius');
+const { fahrenheitToCelcius } = require('./fahrenheit-2-celcius')
 
 describe('the fahrenheit-2-celcius canary spec', () => {
-  it('shows the infrastructure works', () => {
-    expect(true).toBe(true);
-  });
-});
+	it('shows the infrastructure works', () => {
+		expect(true).toBe(true)
+	})
 
-describe('a fahrenheitToCelcius function should', () => {
-  it.todo('return 0 for 32');
-  it.todo('return 10 for 50');
-  it.todo('return 100 for 212');
-  it.todo('return -40 for -40');
-  it.todo('return -273.15 for -459.67');
-});
+	describe.only('a fahrenheitToCelcius function should', () => {
+		it.todo('return 0 for 32')
+		it.todo('return 10 for 50')
+		it.todo('return 100 for 212')
+		it.todo('return -40 for -40')
+		it.todo('return -273.15 for -459.67')
+	})
+})
