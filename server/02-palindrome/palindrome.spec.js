ca72920efbceaed8e9502b2abbe3f0e98593daba
Parinya Samrongsap <parinya.s@cakeboxsolutions.com>
2019-10-31 11:43:15 +0700

    introduce design for palindrome function

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,5 +1,19 @@
+const isPalindrome = text => true
 describe('the palindrome canary spec', () => {
-  it('shows the infrastructure works', () => {
-    expect(true).toBe(true);
-  });
-});
+	it('shows the infrastructure works', () => {
+		expect(true).toBe(true)
+	})
+
+	describe.only('a palindrome function should', () => {
+		it('true for mom', () => {
+			expect(isPalindrome('mom')).toBe(true)
+		})
+
+		it.todo('false for dude')
+		it.todo('true for mom mom')
+		it.todo('false for dad mom')
+		it.todo('true for whitespace')
+		it.todo('error for empty string')
+		it.todo('error for not a string')
+	})
+})
