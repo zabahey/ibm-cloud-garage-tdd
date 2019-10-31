20f8cba124d02b436a52338df754c7f88a52adde
Parinya Samrongsap <parinya.s@cakeboxsolutions.com>
2019-10-31 11:51:05 +0700

    Created palindrome algorithm

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,11 @@
-const isPalindrome = text => true
+const isPalindrome = text => {
+	let reversedText = ''
+	for (let index = text.length - 1; index >= 0; index--) {
+		reversedText += text[index]
+	}
+
+	return reversedText === text
+}
 describe('the palindrome canary spec', () => {
 	it('shows the infrastructure works', () => {
 		expect(true).toBe(true)
@@ -9,9 +16,18 @@ describe('the palindrome canary spec', () => {
 			expect(isPalindrome('mom')).toBe(true)
 		})
 
-		it.todo('false for dude')
-		it.todo('true for mom mom')
-		it.todo('false for dad mom')
+		it('false for dude', () => {
+			expect(isPalindrome('dude')).toBe(false)
+		})
+
+		it('true for mom mom', () => {
+			expect(isPalindrome('mom mom')).toBe(true)
+		})
+
+		it('false for dad mom', () => {
+			expect(isPalindrome('dad mom')).toBe(false)
+		})
+
 		it.todo('true for whitespace')
 		it.todo('error for empty string')
 		it.todo('error for not a string')
