dce43709eb66ad03cb9e7b5078c77987f19ea2a8
Parinya Samrongsap <parinya.s@cakeboxsolutions.com>
2019-10-31 12:20:41 +0700

    group test by behavior

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -5,40 +5,44 @@ describe('the palindrome canary spec', () => {
 		expect(true).toBe(true)
 	})
 
-	describe.only('a palindrome function should', () => {
-		it('true for mom', () => {
-			expect(isPalindrome('mom')).toBe(true)
+	describe('a palindrome function', () => {
+		describe('input with valid text should return', () => {
+			it('true for mom', () => {
+				expect(isPalindrome('mom')).toBe(true)
+			})
+
+			it('false for dude', () => {
+				expect(isPalindrome('dude')).toBe(false)
+			})
+
+			it('true for mom mom', () => {
+				expect(isPalindrome('mom mom')).toBe(true)
+			})
+
+			it('false for dad mom', () => {
+				expect(isPalindrome('dad mom')).toBe(false)
+			})
+
+			it('true for whitespace', () => {
+				expect(isPalindrome('    ')).toBe(true)
+			})
 		})
 
-		it('false for dude', () => {
-			expect(isPalindrome('dude')).toBe(false)
-		})
-
-		it('true for mom mom', () => {
-			expect(isPalindrome('mom mom')).toBe(true)
-		})
-
-		it('false for dad mom', () => {
-			expect(isPalindrome('dad mom')).toBe(false)
-		})
-
-		it('true for whitespace', () => {
-			expect(isPalindrome('    ')).toBe(true)
-		})
-
-		it('return error message "empty strings are not palindromes" for empty string', () => {
-			expect(() => {
-				isPalindrome('')
-			}).toThrowError('empty strings are not palindromes')
-		})
-
-		it.each([[undefined], [null], [123], [true], [{}], [Array(10).fill('aa')]])(
-			'error for not a string %s',
-			input => {
+		describe('input with invalid text should return', () => {
+			it('return error message "empty strings are not palindromes" for empty string', () => {
 				expect(() => {
-					isPalindrome(input)
-				}).toThrowError('input must be a string')
-			}
-		)
+					isPalindrome('')
+				}).toThrowError('empty strings are not palindromes')
+			})
+
+			it.each([[undefined], [null], [123], [true], [Number.NaN], [{}], [Array(10).fill('aa')]])(
+				'error for not a string %s',
+				input => {
+					expect(() => {
+						isPalindrome(input)
+					}).toThrowError('input must be a string')
+				}
+			)
+		})
 	})
 })
