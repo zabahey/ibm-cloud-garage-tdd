ee2ac82c5f67223afc1b9c3920408a424653feba
Parinya Samrongsap <parinya.s@cakeboxsolutions.com>
2019-11-04 12:58:05 +0700

    2 tests passes

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -1,17 +1,26 @@
-describe('the prime factors canary spec', () => {
-  it('shows the infrastructure works', () => {
-    expect(true).toBe(true);
-  });
-});
+const findPrimeFactors = number => {
+	if (number === 2) return [2]
+	return []
+}
 
-describe('a prime factors function should', () => {
-  it.todo('return none for 1');
-  it.todo('return 2 for 2');
-  it.todo('return 3 for 3');
-  it.todo('return 2, 2 for 4');
-  it.todo('return 5 for 5');
-  it.todo('return 2, 3 for 6');
-  it.todo('return 7 for 7');
-  it.todo('return 2, 2, 2 for 8');
-  it.todo('return 3, 3 for 9');
-});
+describe.only('the prime factors canary spec', () => {
+	it('shows the infrastructure works', () => {
+		expect(true).toBe(true)
+	})
+
+	describe('a prime factors function should', () => {
+		it('return none for 1', () => {
+			expect(findPrimeFactors(1)).toEqual([])
+		})
+		it('return 2 for 2', () => {
+			expect(findPrimeFactors(2)).toEqual([2])
+		})
+		it.todo('return 3 for 3')
+		it.todo('return 2, 2 for 4')
+		it.todo('return 5 for 5')
+		it.todo('return 2, 3 for 6')
+		it.todo('return 7 for 7')
+		it.todo('return 2, 2, 2 for 8')
+		it.todo('return 3, 3 for 9')
+	})
+})
